d3b3d10ab7c89ba56fb39f1dea5588bc5d772778
Zaphrog <diegocottin@gmail.com>
2022-02-14 10:45:43 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,39 @@
 //import react into the bundle
+import propTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="four">{props.four}</div>
+			<div className="three">{props.three}</div>
+			<div className="two">{props.two}</div>
+			<div className="one">{props.one}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	four: propTypes.number,
+	three: propTypes.number,
+	two: propTypes.number,
+	one: propTypes.number,
+};
+let counter = 0;
+
+setInterval(function () {
+	const four = Math.floor((counter / 1000) % 10);
+	const three = Math.floor((counter / 100) % 10);
+	const two = Math.floor((counter / 10) % 10);
+	const one = Math.floor((counter / 1) % 10);
+	console.log(four, three, two, one);
+	ReactDOM.render(
+		<SimpleCounter one={one} two={two} three={three} four={four} />,
+		document.querySelector("#app")
+	);
+	counter++;
+}, 1000);
